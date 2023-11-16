b4292b0bde943aa642b539c8493bb24228229bb5
alnmrtnk <alinamartinak@gmail.com>
2023-11-16 23:24:55 +0200

    [fix] removed unnecessary imports

--- src/containers/PizzeriaSimulator/OrdersTable/index.tsx
+++ src/containers/PizzeriaSimulator/OrdersTable/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import style from './style.module.css';
 import DownArrow from '@/icons/DownArrowIcon';
 import UpArrow from '@/icons/UpArrowIcon';
