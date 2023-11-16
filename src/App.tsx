3e5c8d4332ae20cd2bfcf901f2c61ca1f1cdf1d3
alnmrtnk <alinamartinak@gmail.com>
2023-11-16 17:10:06 +0200

    [feat] added orders-table

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Config from './containers/PizzaConfig/Config';
 import './styles/_variables.css';
+import Simulator from './containers/PizzeriaSimulator/Simulator';
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-        <Route path="/" element={<Config/>} />
+          <Route path="/" element={<Config />} />
+          <Route path="/run" element={<Simulator />} />
         </Routes>
     </Router>
     </>
---
+++ src/containers/PizzeriaSimulator/Simulator/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './style.module.css';
+import PizzeriaBackground from '../PizzeriaBackground';
+import OrdersTable from '../OrdersTable';
+
+const Simulator = () => {
+  return (
+    <div className={style.root}>
+      <div className={style.background}>
+        <PizzeriaBackground />
+      </div>
+      <OrdersTable/>
+    </div>
+  );
+};
+
+export default Simulator;
